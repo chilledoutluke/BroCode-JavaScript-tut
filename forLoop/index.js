34db7e22b97b956fa77d43afc17de0b40627d004
chilledoutluke <lukejames.dean08@gmail.com>
2024-08-08 12:49:30 +0200

    created a for loop with continue keyword

--- forLoop/index.js
+++ forLoop/index.js
@@ -2,6 +2,30 @@
 
 //Example 1
 
+/*
 for(let i = 0; i <= 2; i++){
-    console.group(i);
+    console.group("Hello");
+}
+*/    
+
+// You can also count down
+/*
+for(let i = 10; i > 0; i--){
+    console.log(i);
+}
+
+console.log("HAPPY NEW YEAR!");
+*/
+
+//If you ever need to skip an iteration use the continue keyword
+// break keyword is to break out of the loop entirely
+
+for(let i = 1; i <= 20; i++){
+    
+    if(i == 13){
+        continue;
+    }
+    else{
+        console.log(i)
+    }
 }
