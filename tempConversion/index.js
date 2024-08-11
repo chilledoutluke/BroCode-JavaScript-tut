6cf17c1788252b73823d52e0b6dea3960312d653
chilledoutluke <lukejames.dean08@gmail.com>
2024-08-11 14:26:11 +0200

    removed slect unit from #result corrected farenhuit to fahrenhuit

--- tempConversion/index.js
+++ tempConversion/index.js
@@ -1,5 +1,11 @@
 //Temperature COnversion Program
 
+const textBox = document.getElementById("textBox");
+const toFahrenhuit = document.getElementById("toFahrenhuit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+
 function convert(){
     
 }
