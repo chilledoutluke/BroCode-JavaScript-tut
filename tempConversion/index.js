20a14f0aead114b039d227317be3a358c74af814
chilledoutluke <lukejames.dean08@gmail.com>
2024-08-11 14:29:49 +0200

    added if statement to convert() function

--- tempConversion/index.js
+++ tempConversion/index.js
@@ -4,8 +4,18 @@ const textBox = document.getElementById("textBox");
 const toFahrenhuit = document.getElementById("toFahrenhuit");
 const toCelsius = document.getElementById("toCelsius");
 const result = document.getElementById("result");
+let temp;
 
 
 function convert(){
     
+    if(toFahrenhuit.checked){
+
+    }
+    else if(toCelsius.checked){
+
+    }
+    else{
+        result.textContent = "Select a unit";
+    }
 }
