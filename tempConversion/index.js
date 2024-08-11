5a5ab61a462f375bb9cb86bcf62110ac92b4fd78
chilledoutluke <lukejames.dean08@gmail.com>
2024-08-11 14:51:13 +0200

    added to the else if section of the if statement

--- tempConversion/index.js
+++ tempConversion/index.js
@@ -12,10 +12,12 @@ function convert(){
     if(toFahrenhuit.checked){
         temp = Number(textBox.value);
         temp = temp * 9 / 5 + 32;
-        result.textContent = temp + "°F"
+        result.textContent = temp.toFixed(1) + "°F"; // to add  sym hold alt and type 0176 on number pad.
     }
     else if(toCelsius.checked){
-
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9); 
+        result.textContent = temp.toFixed(1) + "°C";
     }
     else{
         result.textContent = "Select a unit";
